0ec3ffbf626c28df79cb3c4fa383bbf88deec9f8
Nathan Howard <Nhoward388@west-mec.org>
2018-05-08 18:07:51 -0700

    Bug Fixing and HTML Updates
    Created the shopping page and its relevant JavaScript and CSS files.
    Displayed the items for sale in the shopping page. Finally I made the
    back button work for donation.js

--- javascript/donation.js
+++ javascript/donation.js
@@ -1,20 +1,22 @@
+var onPayment = false;
+
 var payment = {
     fieldSet: '<fieldset id="payment"></fieldset>',
-    legend: '<legend id="">Payment Details</legend>',
+    legend: '<legend>Payment Details</legend>',
     cardName: "<label class='details' for='name'>Name</label><input class='details' type='text' name='name' id='name'>",
-    cardContainer: "<fieldset id='cards'></label>",    
+    cardContainer: "<fieldset id='cards'></label>",
     cardRow: '<div class="row" id="cardRow"></div>',
     cardColumn: "<div class='col-3 cardCols'></div>",
     card1: "<input name='credit' value='amex' type='radio' required/><img src='images/card_amex.png' alt='American Express'/>",
     card2: '<input name="credit" value="discover" type="radio"/><img src="images/card_discover.png" alt="Discover"/>',
     card3: '<input name="credit" value="master" type="radio"/><img src="images/card_master.png" alt="MasterCard"/>',
     card4: '<input name="credit" value="visa" type="radio"/><img src="images/card_visa.png" alt="Visa"/>',
-    cardNumber: '<label for="cardNumber">Credit Card Number*</label><input name="cardNumber" id="cardNumber" required type="text"pattern="^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$"/>',
+    cardNumber: '<label for="cardNumber">Credit Card Number*</label><input name="cardNumber" id="cardNumber" required type="text" pattern="^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|b?:2131|1800|35\d{3})\d{11})$"/>',
     expirationDate: '<label for="expMonth">Expiration Date*</label><select id="expMonth" name="expMonth"><option value="mm">mm</option><option value="01">01</option><option value="01">02</option><option value="01">03</option><option value="01">04</option><option value="01">05</option><option value="01">06</option><option value="01">07</option><option value="01">08</option><option value="01">09</option><option value="01">10</option><option value="01">11</option><option value="01">12</option></select><span> / </span><select id="expYear" name="expYear"><option value="yy">yy</option><option value="2018">2018</option><option value="2019">2019</option><option value="2020">2020</option><option value="2021">2021</option><option value="2022">2022</option></select>',
     cvc: '<label for="cvc">CVC*</label><input name="cvc" id="cvc" required pattern="^\d{3,4}$" type="text"/>',
     backButton: '<input type="button" name="backButton" id="backButton" value="Back">',
     createPayment: function() {
-        // $("#details").remove();
+        $("#details").remove();
         $("#inputs").append(payment.fieldSet)
         $("#payment").append(payment.legend, payment.cardName, payment.cardContainer, payment.cardNumber, payment.expirationDate, payment.cvc);
         $("#cards").append(payment.cardRow)
@@ -23,30 +25,10 @@ var payment = {
         $(".cardCols")[1].innerHTML = payment.card2;
         $(".cardCols")[2].innerHTML = payment.card3;
         $(".cardCols")[3].innerHTML = payment.card4;
-        $("#backSpace").append(payment.backButton);
+        $("#backSpace").append($(payment.backButton).on("click", details.createDetails));
         $("#submitButton").attr("value", "Submit");
         $("#submitButton").attr("onclick", "function() {alert('Thank you for your donation!')};");
-
-        // !!!bugged code!!!
-
-        // document.getElementById("inputs").innerHTML = payment.fieldset;
-        // document.getElementById("payment").innerHTML = payment.legend;
-        // document.getElementById("payment").innerHTML = payment.cardName;
-        // document.getElementById("payment").innerHTML = payment.cardContainer;
-        // document.getElementById("payment").innerHTML = payment.cardNumber;
-        // document.getElementById("payment").innerHTML = payment.expirationDate;
-        // document.getElementById("payment").innerHTML = payment.cvc;
-        // document.getElementById("cards").innerHTML = payment.cardRow;
-        // document.getElementById("cardRow").innerHTML = payment.cardColumn + payment.cardColumn + payment.cardColumn + payment.cardColumn;
-        // for (var i = 0; i < document.getElementsByClassName("cardCols").length; i++) {
-        //     document.getElementsByClassName("cardCols")[i].innerHTML = payment.card + [i]; 
-        // };
-        // document.getElementById("backSpace").innerHTML = payment.backButton;
-        // document.getElementById("submitButton").value = "Submit";
-        // $(".cardCols")[0].append(payment.card1Label);
-        // $(".cardCols")[1].append(payment.card2Label);
-        // $(".cardCols")[2].append(payment.card3Label);
-        // $(".cardCols")[3].append(payment.card4Label);
+        var onPayment = true;        
     }
 };
 
@@ -55,32 +37,37 @@ var details = {
     legend: '<legend>Donation Details</legend>',
     donation: '<label class="details" for="donationAmount">Donation Amount</label><input class="details" type="number" name="donationAmount" id="donationAmount">',
     name: '<label class="details" for="name">Name</label><input class="details" type="text" name="name" id="name">',
-    date: '<input class="details" type="text" name="date" id="date">',
+    date: '<label class="details" for="date">Date</label><input class="details" type="text" name="date" id="date">',
     createDetails: function() {
+        // document.getElementById("inputs").removeChild(document.getElementById("inputs").childNodes[0]);
+        // document.getElementById("inputs").innerHTML = details.fieldset;
+        // document.getElementById("details").innerHTML = details.donation;
+        // document.getElementById("details").innerHTML = details.name;
+        // document.getElementById("details").innerHTML = details.date;
+        // document.getElementById("submitButton").setAttribute("value", "Next");
+        onPayment = false;        
         $("#payment").remove();
         $("#inputs").append(details.fieldset);
         $("#details").append(details.legend, details.donation, details.name, details.date);
         $("#backButton").remove();
         $("#submitButton").attr("value", "Next");
+        $("#submitButton").attr("onclick", "");
     }
 };
 
 $(document).ready(function() {
-    var onPayment = false;    
+    var details = $("#details").detach()
+    $("#inputs").append(details);
     $("#submitButton").click(function() {
         if (onPayment === false) {
-            var details = $("#details").detach()
             payment.createPayment();
-            onPayment = true;        
-        };
-    });
-    $("#backButton").click(function() {
+            onPayment = true;
+            };
+        });
+    $("#backButton").click = function() {
         if (onPayment === true) {
-            $("#inputs").empty();
-            $("#inputs").append(details);
-            $("#backButton").remove();
-            $("#submitButton").attr("value", "Next");            
-            onPayment = false;      
-        }; 
-    });
+            details.createDetails();
+            onPayment = false;
+        };
+    };
 });
