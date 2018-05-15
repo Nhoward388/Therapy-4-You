650892262a2728cdced41fb60b541bc49cd9eae0
Nathan Howard <Nhoward388@west-mec.org>
2018-05-14 18:13:11 -0700

    Fixed JavaScript bug and added mobile support in the nav bar.

--- javascript/order.js
+++
@@ -1,5 +0,0 @@
-function calcCost(cost) {
-    document.getElementById("value").innerHTML = cost;
-}
-
-$(document).ready(calcCost(cost));
--- javascript/shopping.js
+++ javascript/shopping.js
@@ -1,32 +1,23 @@
 $("#hoodieSubmit").click(function() {
-    var amount = $("#hoodieAmount option:selected");
-    var cost = amount * 50;
-    window.open("order.html", "order.html", "width=600,height=400");    
-    document.getElementById("value").innerHTML = "$" + cost;
+    var amount = $("#hoodieAmount option:selected").val();
+    var cost =  amount * 50;
+    window.open("order.html?cost=" + cost, "order.html", "width=600,height=400");    
 });
 
 $("#bottleSubmit").click(function() {
-    var order = window.open("order.html", "order.html", "width=600,height=400");
-    order.document.getElementById("value").innerText = function() {
-        var amount = $("#bottleAmount option:selected").val();
-        var cost = amount * 10;
-    };
-    order.document.getElementById("value").innerHTML = "$" + cost;
+    var amount = $("#bottleAmount option:selected");
+    var cost =  amount * 50;
+    window.open("order.html?cost=" + cost, "order.html", "width=600,height=400");    
 });
 
 $("#sleeveSubmit").click(function() {
-    var order = window.open("order.html", "order.html", "width=600,height=400");
-    order.document.getElementById("value").innerText = function() {
-        var amount = $("#sleeveAmount option:selected").val();
-        var cost = amount * 40;
-    };
-    order.document.getElementById("value").innerHTML = "$" + cost;
+    var amount = $("#sleeveAmount option:selected");
+    var cost =  amount * 50;
+    window.open("order.html?cost=" + cost, "order.html", "width=600,height=400");    
 });
 
 $("#mugSubmit").click(function() {
-    order.document.getElementById("value").innerText = function() {
-        var amount = $("#mugAmount option:selected").val();
-        var cost = amount * 5;
-    };
-    order.document.getElementById("value").innerHTML = "$" + cost;
+    var amount = $("#mugAmount option:selected");
+    var cost =  amount * 50;
+    window.open("order.html?cost=" + cost, "order.html", "width=600,height=400");    
 });
